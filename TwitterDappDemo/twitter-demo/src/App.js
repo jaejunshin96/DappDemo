c2b1df379fc53401f5ea0b96f49ffcd89f688f98
Jaejun Shin <jaeshinkorean@gmail.com>
2024-11-09 14:34:03 +0900

    twitter dapp

---
+++ TwitterDappDemo/twitter-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TwitterDapp from './components/twitter';
+
+function App() {
+  return (
+    <div className="App">
+		<TwitterDapp />
+    </div>
+  );
+}
+
+export default App;
